0b48c718b9efdf4608030783e1e051d474b812f3
JoyceKuohMoukouri <136962595+joycekuohmoukouri@users.noreply.github.com>
2023-08-16 10:51:23 +0200

    Create API_1.py

---
+++ API_1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import re
+import seaborn as sns
+import missingno as msno
+import clean #mon module de fontions qui me permettent de nettoyer un dataframe
+import plot_
+import joblib
+import os
+import MyModule_p7
+
+#-------------------------Filter warnings-------------------------
+import warnings
+warnings.filterwarnings('ignore')
+! pip install category_encoders --quiet
+! pip install shap --quiet
+! pip install Flask
