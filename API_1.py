190489e8ecf7dd4d499c3543def25640b4683666
JoyceKuohMoukouri <136962595+joycekuohmoukouri@users.noreply.github.com>
2023-08-16 11:03:13 +0200

    Update API_1.py

--- API_1.py
+++ API_1.py
@@ -17,3 +17,18 @@ warnings.filterwarnings('ignore')
 pip install category_encoders --quiet
 pip install shap --quiet
 pip install Flask
+
+from flask import Flask, request, jsonify
+
+
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()  # Get JSON data from the request
+    prediction = your_ml_model_module.predict(data)  # Use your ML model for prediction
+    return jsonify({'prediction': prediction})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Run the app on localhost at port 5000
