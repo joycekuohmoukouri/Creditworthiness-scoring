fd944a808766c633165649f119bbfde6cbc54986
JoyceKuohMoukouri <joycebert@icloud.com>
2023-09-11 06:44:08 +0200

    11_09_2023_0604

--- API_1.py
+++ API_1.py
@@ -6,8 +6,12 @@ import plotly.graph_objs as go
 import plotly.express as px
 from flask import Flask, request, jsonify, send_file, render_template, redirect
 import subprocess
-import os
 import json
+import joblib
+import MyModule_p7
+import shap  # Import the 'shap' module
+import os
+from projet7package.frequency_encode import frequency_encode
 
 ## Création de l'API
 
@@ -23,16 +27,42 @@ def welcome():
 # Define a route for the welcome page
 @app.route('/Dashboard/',methods=['GET','POST'])
 def Dashboard():
+    from projet7package.frequency_encode import frequency_encode
     # Receive client ID from the form submission
     client_id = request.form.get('client_id')
-    api_url = 'https://pacific-badlands-33124-c377cfc7c668.herokuapp.com/predict/'
-    headers = {'Content-Type': 'application/json'} 
-    response = requests.post(api_url, json={'client_id': client_id}, headers=headers)
-    data = response.json()
-      # Store the data in the global variable
-    global dashboard_data
-    dashboard_data = data
-    return redirect("https://dashboard-projet-7-jkm-bcf05f5e28fb.herokuapp.com")
+    if client_id is not None:
+        # Chargement des données client using your module
+        client_data = MyModule_p7.get_client_data(client_id)
+        # Transformation
+        loaded_preprocess = MyModule_p7.preprocess_model()
+        df_client_pp = loaded_preprocess.transform(client_data)
+        classification_model = joblib.load('LightGBM_bestmodel.pkl')
+        #Prédiction
+        prediction = classification_model.predict(df_client_pp)
+        proba = classification_model.predict_proba(df_client_pp)
+        score = int(round((proba[0][0])*100)) #probabilité complémentaire
+        # Feature analysis using SHAP values
+        SV, df_client_pp = MyModule_p7.feat_local(df_client_pp)
+        # Dataframe sv_df
+        sv_df = pd.DataFrame(columns=['Class_0', 'Class_1'], index=df_client_pp.columns)
+        sv_df['Class_0'] = SV[0].T
+        sv_df['Class_1'] = SV[1].T
+        sv_df = sv_df.reset_index()
+        sv_df = sv_df.to_dict()
+
+              # Store the data in the global variable
+    #global dashboard_data
+    #dashboard_data = data
+
+        df_client_pp = df_client_pp.to_dict()
+
+        return jsonify({'client_id': client_id,
+        'score': score,
+        'feat_imp' :sv_df,
+        'client_data' : df_client_pp
+    })
+
+
 
 
 @app.route('/Dashboard_st/',methods=['GET',])
