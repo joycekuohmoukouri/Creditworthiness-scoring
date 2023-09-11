65da58a298efa8bafb9f8732b24cfe1369594a3f
JoyceKuohMoukouri <joycebert@icloud.com>
2023-09-11 07:49:03 +0200

    11_09_2023_0749

--- api-2-projet-7/test_pack.py
+++
@@ -1,9 +0,0 @@
-# Test du package projet-7-package
-import pandas as pd
-import MyModule_p7
-from projet7package.frequency_encode import frequency_encode
-
-data = pd.read_csv('client_test_db.csv', nrows = 1)
-loaded_preprocess = MyModule_p7.preprocess_model()
-data_bis = loaded_preprocess.transform(data)
-print(data_bis)
